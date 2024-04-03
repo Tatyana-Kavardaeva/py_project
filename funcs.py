0c8cee9b7536698112798ee8f86fbac3a37d6c87
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-04-04 01:29:36 +0300

    Change funcs.py

--- funcs.py
+++ funcs.py
@@ -6,6 +6,14 @@ def count_sum(num1, num2):
         return "Результат не известен. Число не должно содержать буквы или символы"
 
 
+def count_dif(num1, num2):
+    """реализует вычитание"""
+    if num1.isdigit() is True and num2.isdigit() is True:
+        return f"Разность числел равна {int(num1) - int(num2)}"
+    else:
+        return "Результат не известен. Число не должно содержать буквы или символы"
+
+
 number1 = input("Введите число")
-number2 = input("На сколько Вы хотите его увеличить?")
-print(count_sum(number1, number2))
+number2 = input("На сколько Вы хотите его уменьшить?")
+print(count_dif(number1, number2))
