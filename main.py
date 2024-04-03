23ecb021d3993e83970b353c9bf716f246ad92de
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-04-04 01:46:09 +0300

    Import count_dif in main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from funcs import count_sum
+from funcs import count_sum, count_dif
+
 
 if __name__ == '__main__':
     num1 = input()
     num2 = input()
     print(count_sum(num1, num2))
+    print(count_dif(num1, num2))
